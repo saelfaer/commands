511579274fb2b63c3530b0f7853bbd937c1266b3
Sander Houttekier <sander.houttekier@gmail.com>
2019-11-18 15:32:28 +0100

    feat(up): add command to traverse up the path

--- commands.sh
+++ commands.sh
@@ -1,2 +1,3 @@
 source ~/projects/commands/git_clean_local_branches.sh
 source ~/projects/commands/git_clean_untracked_safely.sh
+source ~/projects/commands/up.sh
---
+++ up.sh
@@ -0,0 +1,13 @@
+function up {
+    dir=""
+    if [[ $1 =~ ^[0-9]+$ ]]; then
+        x=0
+        while [ $x -lt ${1:-1} ]; do
+            dir=${dir}../
+            x=$(($x+1))
+        done
+    else
+         dir=..
+    fi
+    cd "$dir";
+}
